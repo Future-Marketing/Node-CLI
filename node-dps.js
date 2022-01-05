012a2fcc3822c95b5d0a7fac9c544d3cf687071d
Noor Latif <noor.crystal@gmail.com>
2022-01-05 08:02:36 +0100

    Create node-dps.js

---
+++ node-dps.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var provisioningServiceClient = require('azure-iot-provisioning-service').ProvisioningServiceClient;
+
+var serviceClient = provisioningServiceClient.fromConnectionString(process.argv[2]);
+var endorsementKey = process.argv[3];
+
+var enrollment = {
+  registrationId: 'first',
+  attestation: {
+    type: 'tpm',
+    tpm: {
+      endorsementKey: endorsementKey
+    }
+  }
+};
+
+serviceClient.createOrUpdateIndividualEnrollment(enrollment, function (err, enrollmentResponse) {
+  if (err) {
+    console.log('error creating the individual enrollment: ' + err);
+  } else {
+    console.log("enrollment record returned: " + JSON.stringify(enrollmentResponse, null, 2));
+  }
+});
